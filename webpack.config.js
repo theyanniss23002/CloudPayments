d4afe9f2f0301e045ba4f8ef9754c7104292c180
theyanniss23002 <mr.artur23002@gmail.com>
2020-07-25 03:52:33 +0300

    added to vendor configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,61 +4,75 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.[chunkhash].js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'images',
-                    name: '[path][name].[ext]',
-                },
-            },
-            {
-                test: /\.(ttf|otf)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.sass$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: { sourceMap: true }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: { sourceMap: true, config: { path: './postcss.config.js' } }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: { sourceMap: true }
-                    }
-                    ]
-            }
-                ]
-            },
-    devServer: {
-        port: 3000
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'app.css'
-        })
-    ],
+	mode: 'development',
+	entry: './src/app.js',
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'public'),
+	},
+	resolve: {
+		alias: {
+			'@images': path.resolve(__dirname, 'src/images'),//not working
+			'@': path.resolve(__dirname, 'src'),//not working
+			'@css': path.resolve(__dirname, 'src/css'),//not working
+		},
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader']
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				loader: 'file-loader',
+				options: {
+					outputPath: 'public',
+					publicPath: 'images',
+					name: '@images/[path][name].[ext]',//not working
+				},
+			},
+			{
+				test: /\.(ttf|otf)$/,
+				use: ['file-loader']//not working
+			},
+			{
+				test: /\.sass$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { sourceMap: true }
+					},
+					{
+						loader: 'postcss-loader',
+						options: { sourceMap: true, config: { path: './postcss.config.js' } }
+					},
+					{
+						loader: 'sass-loader',
+						options: { sourceMap: true }
+					}
+				]
+			}
+		]
+	},
+	devServer: {
+		port: 4200
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		}),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].[contenthash].css'
+		})
+	],
 }
