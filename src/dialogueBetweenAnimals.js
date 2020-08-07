7800a99312023053bd64e84561476c9686040f6b
theyanniss23002 <mr.artur23002@gmail.com>
2020-08-08 02:01:39 +0300

    added function from modal window

--- src/dialogueBetweenAnimals.js
+++ src/dialogueBetweenAnimals.js
@@ -2,9 +2,28 @@ import {gsap} from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
 import $ from 'jquery'
 
-setTimeout(function() {
-  $('body').addClass('loaded');
-}, 1000);
+
+$(document).ready(function () {
+  // Fakes the loading setting a timeout
+  setTimeout(function() {
+    $('body').addClass('loaded');
+  }, 1000);
+
+  $(".coin-wrap, .boom-wrap, .trash-wrap, .cube-wrap").on('click', function () {
+    let data = $(this).data('class');
+    $('body').css('perspective', 'none');
+    $('.character-cell').fadeOut(500);
+    $(".slider").addClass('active');
+    $(`.${data}`).fadeIn(500);
+  });
+  $(".slide-close").on('click', function () {
+    $('body').css('perspective', '1px');
+    $(".slider").removeClass('active');
+    $('.slideshow__card').fadeOut(500);
+    $('.character-cell').fadeIn(500)
+  });
+});
+
 
 gsap.registerPlugin(ScrollTrigger);
 
