6ec0009268c51670323b22428b3ecc903456c1af
Lenar <linz14@mail.ru>
2020-08-18 10:08:55 +0300

    mobile bugfix

--- src/verticalAdaptive.js
+++ src/verticalAdaptive.js
@@ -2,32 +2,37 @@ let width = window.innerWidth;
 import $ from 'jquery'
 
 window.addEventListener("orientationchange", checkOrientation);
-window.addEventListener("resize", checkOrientation, false);
+// window.addEventListener("resize", checkOrientation, false);
 document.addEventListener("DOMContentLoaded", checkOrientation);
 
 
 function checkOrientation() {
-    if (window.orientation === 0 && width < 1200) {
-        document.getElementById('rotation').style.display = "flex";
-        document.querySelector('.universe').style.display = "none";
-    } else {
-        document.getElementById('rotation').style.display = "none";
-        document.querySelector('.universe').style.display = "block";
-    }
+  if (window.orientation === 0 && width < 1200) {
+    document.getElementById('rotation').style.display = "flex";
+    document.querySelector('.universe').style.display = "none";
+  } else {
+    document.getElementById('rotation').style.display = "none";
+    document.querySelector('.universe').style.display = "block";
+  }
 }
 
-let cientHeight = document.documentElement.clientHeight;
+// let cientHeight = document.documentElement.clientHeight;
 
-if (width < 1200) {
-    $('.intro').css({
-        'height': cientHeight,
-        'min-height': 'unset'
-    });
+// if (width < 1200) {
+//   $('.intro').css({
+//     'height': cientHeight,
+//     'min-height': 'unset'
+//   });
 
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
-}
+  // let vh = window.innerHeight * 0.01;
+  // document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // window.addEventListener('resize', () => {
+  //   let vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // });
+// }
+
+// window.addEventListener('resize', () => {
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
